44dfd0cae31bc300ee2a2530b2eabf9c3483a030
Sharifzoda Sharif <sharifzodastudy@gmail.com>
2023-06-01 18:08:58 +0500

    2023-06-01

--- internal/handler/api_test.go
+++ internal/handler/api_test.go
@@ -96,3 +96,65 @@ func TestHandler_createTask(t *testing.T) {
 		})
 	}
 }
+
+func TestHandler_getTasks(t *testing.T) {
+	type mockBehavior func(a *mock_service.MockAuthorization, td *mock_service.MockTodoTask, token string)
+
+	testTable := []struct {
+		name                 string
+		mockBehavior         mockBehavior
+		expectedStatusCodes  int
+		expectedResponseBody string
+		headerName           string
+		headerValue          string
+	}{
+		{
+			name: "OK",
+			mockBehavior: func(a *mock_service.MockAuthorization, td *mock_service.MockTodoTask, token string) {
+				a.EXPECT().ParseToken(token).Return(1, nil).AnyTimes()
+				td.EXPECT().GetAll(1).Return(model.Tasks{
+					{Id: 1, Name: "mathematics", Description: "study hard", Done: false, IsActive: true,
+						Deadline: "2022-05-23T00:00:00Z", Username: "Sharif"},
+					{Id: 2, Name: "go", Description: "study smart", Done: false, IsActive: true,
+						Deadline: "2022-06-22T00:00:00Z", Username: "Sharif"},
+				}, nil).AnyTimes()
+			},
+			expectedStatusCodes: 200,
+			expectedResponseBody: `{"tasks":[{"id":1,"name":"mathematics","description":"study hard","done":false,` +
+				`"is_active":true,"deadline":"2022-05-23T00:00:00Z","username":"Sharif"},` +
+				`{"id":2,"name":"go","description":"study smart","done":false,"is_active":true,` +
+				`"deadline":"2022-06-22T00:00:00Z","username":"Sharif"}]}`,
+			headerName:  "Authorization",
+			headerValue: "Alif token",
+		},
+	}
+
+	for _, testCase := range testTable {
+		t.Run(testCase.name, func(t *testing.T) {
+			c := gomock.NewController(t)
+			defer c.Finish()
+
+			auth := mock_service.NewMockAuthorization(c)
+			todo := mock_service.NewMockTodoTask(c)
+
+			split := strings.Split(testCase.headerValue, " ")
+
+			testCase.mockBehavior(auth, todo, split[1])
+
+			handler := NewHandler(auth, todo)
+
+			gin.SetMode(gin.ReleaseMode)
+			r := gin.New()
+			r.GET("/getTasks", handler.tokenAuthMiddleware, handler.getTasks)
+
+			w := httptest.NewRecorder()
+			req := httptest.NewRequest("GET", "/getTasks", nil)
+			req.Header.Set(testCase.headerName, testCase.headerValue)
+
+			r.ServeHTTP(w, req)
+
+			assert.Equal(t, w.Code, testCase.expectedStatusCodes)
+			assert.Equal(t, w.Body.String(), testCase.expectedResponseBody)
+		})
+	}
+}
